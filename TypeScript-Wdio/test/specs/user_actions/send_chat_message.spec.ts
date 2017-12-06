2601a1c2c8561b615fbcdd03c78cb2a4bb4992a9
Akshay Sud <sudakshay@gmail.com>
2017-12-06 21:55:20 +1300

    :art:

--- TypeScript-Wdio/test/specs/user_actions/send_chat_message.spec.ts
+++ TypeScript-Wdio/test/specs/user_actions/send_chat_message.spec.ts
@@ -13,22 +13,22 @@ const common = new CommonComponents();
 
 describe('Send chat message to a friend', () => {
 
-    const data = UserData.generateUserData();
+	const data = UserData.generateUserData();
 
-    beforeAll(function () {
-        console.log('Log in to facebook');
-        BrowserHelper.navigate(login.loginUrl(), login.welcomePageElements.Email.selector);
-        login.welcomePage.enterUserCredentials(data);
-        login.welcomePage.clickLogin(user.homePageElements.SearchBar.selector);
-    });
+	beforeAll(function () {
+		console.log('Log in to facebook');
+		BrowserHelper.navigate(login.loginUrl(), login.welcomePageElements.Email.selector);
+		login.welcomePage.enterUserCredentials(data);
+		login.welcomePage.clickLogin(user.homePageElements.SearchBar.selector);
+	});
 
-    given`I navigate to news feed`(() => user.homePage.navigateToNewsFeed(user.homePageElements.SearchBar.selector))
-    when`I select the buddy list`(() => common.chatBox.clickChatBar());
-    then`My buddy list is displayed`(() => common.chatBoxAssertions.verifyBuddyList());
+	given`I navigate to news feed`(() => user.homePage.navigateToNewsFeed(user.homePageElements.SearchBar.selector))
+	when`I select the buddy list`(() => common.chatBox.clickChatBar());
+	then`My buddy list is displayed`(() => common.chatBoxAssertions.verifyBuddyList());
 
-    when`I select a friend to chat with`(() =>  common.chatBox.selectHumanFromChatList())
-        and`I send my chat message ${data.chatMessage}`((message) => common.chatBox.sendDirectMessage(message));
-   // then`My message is delivered successfully`
+	when`I select a friend to chat with`(() => common.chatBox.selectHumanFromChatList())
+	and`I send my chat message ${data.chatMessage}`((message) => common.chatBox.sendDirectMessage(message));
+	// then`My message is delivered successfully`
 
-    browser.end();
+	browser.end();
 });
