4c737ae19bcc7a0367c6125216616d8a6bb77fb4
Akshay Sud <sudakshay@gmail.com>
2018-04-09 10:22:12 +1200

    meh

--- test/shared/global/test_data.ts
+++ test/shared/global/test_data.ts
@@ -25,7 +25,7 @@ export class UserData {
 			chatMessage: 'Automated Message - not sent by human',
 			pollTitle: 'My awesome poll',
 			pollOptionOne: 'To be',
-			pollOptionTwo: 'Or not to be'
+			pollOptionTwo: 'Or not to be travis'
 		}
 
 		console.log(`Test Data: ${JSON.stringify(data)}`);
